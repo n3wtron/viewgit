1db8095dfa70e5a0e03a1930287aac3e719a4101
n3wtron <n3wtron@gmail.com>
2011-09-27 12:03:34 +0200

    Added geshi highlighter

--- templates/blob.php
+++ templates/blob.php
@@ -1,3 +1,6 @@
+<?php
+	include_once "./geshi/geshi.php";
+?>
 <h2>Last commit for <?php echo $page['path']; ?>: <a href="<?php echo makelink(array('a' => 'commit', 'p' => $page['project'], 'h' => $page['lastlog']['h'])); ?>"><?php echo htmlentities_wrapper($page['lastlog']['h']); ?></a></h2>
 <h1><?php echo htmlentities_wrapper($page['lastlog']['message_firstline']); ?></h1>
 <div class="authorinfo">
@@ -11,17 +14,22 @@ echo ' ['. $page['lastlog']['author_datetime'] .']';
 <?php echo htmlentities_wrapper($page['lastlog']['message_full']); ?>
 </pre>
 </div>
-<div class="file">
+<div class="file" style="overflow:auto;">
 <?php
-if (isset($page['html_data'])) {
-	echo $page['html_data'];
-}
-else {
-?>
-<pre>
-<?php echo htmlspecialchars($page['data']); ?>
-</pre>
+	if (isset($page['html_data'])) {
+		echo $page['html_data'];
+	}
+	else {
+?> 
+<pre><?php
+		//retrieve file extension
+		$ext = substr($f,strrpos($f,'.')+1,strlen($f)-strrpos($f,'.'));
+		$geshi = new GeSHi ($page['data'],$ext);
+		$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS);
+		$geshi->set_keyword_group_style(3, 'color: black; ', true);
+		echo $geshi->parse_code();
+	?></pre>
 </div>
 <?php
-}
+	}
 ?>
